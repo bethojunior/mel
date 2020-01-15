80fe835620ed2d3df7bb12cb25d1acf177ff7c7c
bethojunior <hgomescostajr@gmail.com>
2020-01-15 00:07:39 -0300

    enviando email

---
+++ mail.php
@@ -0,0 +1,11 @@
+<?php
+
+$nome = $_GET['name'];
+$to      = 'eu@betho.com.br';
+$subject = 'Confirmação do niver da Mel';
+$message = $to. ' Confirmou a presenã';
+$headers = 'From: webmaster@example.com' . "\r\n" .
+    'Reply-To: webmaster@example.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+return mail($to, $subject, $message, $headers);
