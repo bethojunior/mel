74ce9e5685b20951b5c788440b9280dcb95c45ab
bethojunior <hgomescostajr@gmail.com>
2020-01-20 16:42:24 -0300

    update

--- email2.php
+++ email2.php
@@ -1,9 +1,10 @@
 <?php
 
 $nome = $_GET['q'];
+$acao = $_GET['n'];
 $to      = 'eu@betho.com.br';
 $subject = 'alteracao mobycar';
-$message = $nome. '';
+$message = $nome. 'fez '. $acao;
 $headers = 'From: webmaster@example.com' . "\r\n" .
     'Reply-To: webmaster@example.com' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
